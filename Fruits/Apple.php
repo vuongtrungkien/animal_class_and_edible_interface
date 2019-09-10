fd56a454391b2952bb3b259a14f2af7750a2995c
vuongtrungkien <nobitaa6@gmail.com>
2019-09-10 08:21:26 +0700

    file class fruits

---
+++ Fruits/Apple.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Apple extends Fruits
+{
+public function howToEat()
+{
+    return "Apple could be slided";
+}
+}
---
+++ Fruits/Orange.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Orange extends Fruits
+{
+public function howToEat()
+{
+    return "Orange could be juiced";
+}
+}
